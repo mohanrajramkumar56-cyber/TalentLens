370abd1b7fce81e5238f567cfdbaebe3a6dfc737
mohanraj101 <mohanrajr@datapattern.ai>
2026-09-10 23:08:32 +0530

    Fix langchain.chains import - use full path

--- api/index.py
+++ api/index.py
@@ -11,7 +11,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 
 app = FastAPI(title="TalentLens")
--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_community.vectorstores import FAISS
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains.conversational_retrieval.base import ConversationalRetrievalChain
 from langchain.memory import ConversationBufferMemory
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
